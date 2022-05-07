40043fba5a44c47a4da3e7aaab8360ca67fd9dea
Cx3n1 <lukagobronidze@gmail.com>
2022-05-07 22:38:49 +0400

    android midterm

---
+++ app/src/main/java/dev/cx3n1/midterm1/MainActivity.kt
@@ -0,0 +1,60 @@
+package dev.cx3n1.midterm1
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var nameInputField : EditText
+    lateinit var confirmInputButton : ImageButton
+    lateinit var profileIconButton : ImageButton
+
+    lateinit var sharedPreference: SharedPreferences
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        sharedPreference = getSharedPreferences("SHARED_PREFS", Context.MODE_PRIVATE)
+
+        nameInputField = findViewById(R.id.etxt_nameInput)
+        confirmInputButton = findViewById(R.id.imgBtn_confirmInput)
+        profileIconButton = findViewById(R.id.imgBtn_profileIcon)
+
+        confirmInputButton.setOnClickListener {
+            saveNewNameInSharedPreferences()
+            startProfileActivity()
+        }
+
+        profileIconButton.setOnClickListener{
+            startProfileActivity()
+        }
+    }
+
+
+    private fun startProfileActivity() {
+        startActivity(Intent(this, ProfileActivity::class.java));
+    }
+
+    @SuppressLint("CommitPrefEdits")
+    private fun saveNewNameInSharedPreferences() {
+        val inputtedUsername : String = nameInputField.text.toString()
+
+        if(isEmptyOrBlank(inputtedUsername)) return
+
+        val editor = sharedPreference.edit()
+        editor.putString("Username", inputtedUsername)
+        editor.apply()
+    }
+
+
+    private fun isEmptyOrBlank(inputtedUsername: String) =
+        inputtedUsername.isBlank() || inputtedUsername.isEmpty()
+}
---
+++ app/src/main/java/dev/cx3n1/midterm1/ProfileActivity.kt
@@ -0,0 +1,33 @@
+package dev.cx3n1.midterm1
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+        val sharedPreference = getSharedPreferences("SHARED_PREFS", Context.MODE_PRIVATE)
+        val profTextView = findViewById<TextView>(R.id.txt_profileText);
+        val backToMainButton = findViewById<ImageButton>(R.id.imgBtn_backToMain)
+
+        val greetingText = "Hello, " + sharedPreference.getString("Username", "Your name");
+
+        profTextView.setText(greetingText);
+
+        backToMainButton.setOnClickListener{
+            goBackToMainActivity();
+        }
+    }
+
+    private fun goBackToMainActivity() {
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+
+}
